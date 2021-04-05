ca92e2cb67c3036c5c3df9d653851d677a3a56eb
Listeriaa <laeti.rd@gmail.com>
2021-04-05 18:48:52 +0200

    a gérer close menu avec click window

--- js/app.js
+++ js/app.js
@@ -7,11 +7,16 @@ const app = {
     },
     handleHamburgerMenu: function(){
         let hbgButton = document.querySelector(".hbg-menu");
-        let h1 = document.querySelector('h1');
+        
         let nav = document.querySelector('nav');
         nav.classList.toggle('open');
         hbgButton.classList.toggle('open');
-        h1.classList.toggle('open');
+       
+        // window.addEventListener('click', (()=> {
+        //     nav.classList.remove('open');
+        // hbgButton.classList.remove('open');
+        
+        // }))
     }
 }
 
