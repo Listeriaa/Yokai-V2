28342b74216997658e6183abbe75fb72f6a442f1
Listeriaa <laeti.rd@gmail.com>
2021-04-15 14:06:48 +0200

    page yokai ok en resp

--- inc/yokai.tpl.php
+++ inc/yokai.tpl.php
@@ -6,20 +6,23 @@
             <img src="images/<?=$yokais[$yokai]["image"]?>" alt="illustration de <?=$yokais[$yokai]["nom"]?>">
         </div>
         <div class="descriptif_article_responsive">
-            <p><span>Traduction :</span>  <?=$yokais[$yokai]["traduction"]?></p>
-
-            <p><span>Habitat</span> : <?=$yokais[$yokai]["habitat"]?></p>
+            <h3>Traduction :</h3>
+            <p><?=$yokais[$yokai]["traduction"]?></p>
+            <h3>Habitat :</h3>
+            <p><?=$yokais[$yokai]["habitat"]?></p>
             <?php
             //Je n'affiche ce paragraphe que si la clé "origine" existe dans le sous-tableau du yokai
             if (array_key_exists('origine', $yokais[$yokai])):
             ?>
-            <p><span>Origines</span> : <?=$yokais[$yokai]["origine"]?></p>
+            <h3>Origines :</h3>
+            <p><?=$yokais[$yokai]["origine"]?></p>
             <?php
             endif;
             ?>
-            <p><span>Apparence</span> : <?=$yokais[$yokai]["apparence"]?></p>
-        
-            <p><span>Comportement</span> : <?=$yokais[$yokai]["comportement"]?></p>
+            <h3>Apparence :</h3>
+            <p><?=$yokais[$yokai]["apparence"]?></p>
+            <h3>Comportement :</h3>
+            <p><?=$yokais[$yokai]["comportement"]?></p>
         </div>
 
     </article>
